05e5cabb0e90636517e33e1322a79af938c295ba
Lau91 <kirtas91@gmail.com>
2025-01-20 12:16:33 +0100

    Add files via upload
    Dynamic component for creating badges using an array of strings, made in Vue3 and boostrap5.
    
    Usage: 
    1.Add component in folder components
    
    2.In the view add the component:
    <BadgetComponent :array-badgets="arrayBadgets" />
    import BadgetComponent from '../components/BadgetComponent.vue';

---
+++ BadgetComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <span v-for="badget in arrayBadgets" :key="badget.id" class="badge badge-pill">{{badget}}</span>
+</template>
+
+<script setup>  
+defineProps({
+  arrayBadgets: {
+    type: Array,
+    required: true
+  }
+  
+})
+</script>
+
+<style scoped>
+.badge{
+    background-color: #6957E3;
+    padding: 8px;
+    border-radius: 20px;
+    margin-right: 10px;
+    margin-top: 5px;
+}
+</style>
